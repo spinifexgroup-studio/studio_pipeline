66187d0ceeb219fe9a390dd6ee8266cd112d4d90
Daniel Harkness <daniel.harkness@spinfiexgroup.com>
2013-06-27 17:00:22 +1000

    SetProxies for nuke
    made script that can be used to set proxies generated by deadline proxy
    script

---
+++ nuke_scripts_develop/SetProxies.py
@@ -0,0 +1,38 @@
+import nuke
+import os
+import nukescripts
+
+nodes = nuke.selectedNodes()
+if nodes:
+	panel = nuke.Panel('Proxy Type')
+	panel.addEnumerationPulldown( 'Proxy Type', '"Sub Directory: filedir/proxy/filename" "Same Directory: filedir/filename_proxy" "Parent Directory: filedir_proxy/filename"' )
+	panel.show()
+
+	proxyType = 'same'
+
+	if panel.value('Proxy Type') == "Sub Directory: filedir/proxy/filename":
+		proxyType = 'sub'
+	if panel.value('Proxy Type') == "Same Directory: filedir/filename_proxy":
+		proxyType = 'same'
+	if panel.value('Proxy Type') == "Parent Directory: filedir_proxy/filename":
+		proxyType = 'parent'
+
+	for node in nodes:
+		if node.Class() == 'Read':
+			filePath = node.knob('file').value().replace('\\','/')
+			proxyPath = ''
+			fileDir = os.path.dirname(path)
+			fileBase = os.path.basename(path)
+			if proxyType == 'sub':
+				proxyPath = os.path.join ( fileDir, 'proxy', fileBase )
+			
+			if proxyType == 'same':
+				fileBase = fileBase.split('.%',1)[0] + '_proxy' + '.%' + fileBase.split('.%',1)[1]
+				proxyPath = os.path.join ( fileDir, fileBase )
+			
+			if proxyType == 'parent':
+				proxyPath = os.path.join ( os.path.dirname( fileDir ), os.path.basename( fileDir )+'_proxy', fileBase )
+		
+			node.knob('proxy').setValue( proxyPath)
+else:
+	nuke.message('Nothing Selected!')
